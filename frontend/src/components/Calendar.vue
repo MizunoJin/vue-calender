5001dc5be49347d22d20ce26cd29bc842672ed4f
fantagreep <bump.lk_k.1215@icloud.com>
2021-09-20 16:53:57 +0900

    Display event

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -1,31 +1,67 @@
 <template>
   <div>
-    <p>{{ events }}</p>
-    <ul>
-      <li v-for="event in events" :key="event.id">
-        {{ event.name }}
-      </li>
-    </ul>
-    <button type="submit" @click="fetchEvents()">fetchEvents</button>
+    <v-sheet height="6vh" class="d-flex align-center">
+      <v-btn outlined small class="ma-4" @click="setToday">今日</v-btn>
+      <v-btn icon @click="$refs.calendar.prev()">
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+      <v-btn icon @click="$refs.calendar.next()">
+        <v-icon>mdi-chevron-right</v-icon>
+      </v-btn>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-sheet>
+    <v-sheet height="94vh">
+      <v-calendar
+        ref="calendar"
+        v-model="value"
+        :events="events"
+        @change="fetchEvents"
+        locale="ja-jp"
+        :day-format="(timestamp) => new Date(timestamp.date).getDate()"
+        :month-format="(timestamp) => new Date(timestamp.date).getMonth() + 1 + '/'"
+        @click:event="showEvent"
+      ></v-calendar>
+    </v-sheet>
 
-    <CalendarDetails />
+    <v-dialog :value="event !== null">
+      <div v-if="event !== null">
+        <v-card>
+          <h1>イベント詳細</h1>
+          <p>name: {{ event.name }}</p>
+          <p>start: {{ event.start.toLocaleString() }}</p>
+          <p>end: {{ event.end.toLocaleString() }}</p>
+          <p>timed: {{ event.timed }}</p>
+          <p>description: {{ event.description }}</p>
+          <p>color: {{ event.color }}</p>
+        </v-card>
+      </div>
+    </v-dialog>
   </div>
 </template>
 
 <script>
+import { format } from 'date-fns';
 import { mapGetters, mapActions } from 'vuex';
-import CalendarDetails from './CalendarDetails.vue';
 
 export default {
   name: 'Calendar',
-  components: {
-    CalendarDetails,
-  },
+  data: () => ({
+    value: format(new Date(), 'yyyy/MM/dd'),
+  }),
   computed: {
-    ...mapGetters('events', ['events']),
+    ...mapGetters('events', ['events', 'event']),
+    title() {
+      return format(new Date(this.value), 'yyyy年 M月');
+    },
   },
   methods: {
-    ...mapActions('events', ['fetchEvents']),
+    ...mapActions('events', ['fetchEvents', 'setEvent']),
+    setToday() {
+      this.value = format(new Date(), 'yyyy/MM/dd');
+    },
+    showEvent({ event }) {
+      this.setEvent(event);
+    },
   },
 };
 </script>
--- frontend/src/store/modules/events.js
+++ frontend/src/store/modules/events.js
@@ -4,14 +4,31 @@ const apiUrl = 'http://localhost:3000';
 
 const state = {
   events: [],
+  event: null,
 };
 
 const getters = {
-  events: (state) => state.events,
+  events: (state) =>
+    state.events.map((event) => {
+      return {
+        ...event,
+        start: new Date(event.start),
+        end: new Date(event.end),
+      };
+    }),
+  event: (state) =>
+    state.event
+      ? {
+          ...state.event,
+          start: new Date(state.event.start),
+          end: new Date(state.event.end),
+        }
+      : null,
 };
 
 const mutations = {
   setEvents: (state, events) => (state.events = events),
+  setEvent: (state, event) => (state.event = event)
 };
 
 const actions = {
@@ -19,6 +36,9 @@ const actions = {
     const response = await axios.get(`${apiUrl}/events`);
     commit('setEvents', response.data); // mutationを呼び出す
   },
+  setEvent({ commit }, event) {
+    commit('setEvent', event);
+  }
 };
 
 export default {
