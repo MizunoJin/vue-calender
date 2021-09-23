6cf19961af9646a3fdb39cdbb22d35c636418735
fantagreep <bump.lk_k.1215@icloud.com>
2021-09-23 11:24:57 +0900

    Add time component

--- frontend/src/store/modules/events.js
+++ frontend/src/store/modules/events.js
@@ -1,37 +1,25 @@
 import axios from 'axios';
+import { serializeEvent } from '../../functions/serializers';
 
 const apiUrl = 'http://localhost:3000';
 
 const state = {
   events: [],
   event: null,
-  isEditMode: false
+  isEditMode: false,
 };
 
 const getters = {
-  events: (state) =>
-    state.events.map((event) => {
-      return {
-        ...event,
-        start: new Date(event.start),
-        end: new Date(event.end),
-      };
-    }),
-  event: (state) =>
-    state.event
-      ? {
-          ...state.event,
-          start: new Date(state.event.start),
-          end: new Date(state.event.end),
-        }
-      : null,
-  isEditMode: state => state.isEditMode
+  events: (state) => state.events.map((event) => serializeEvent(event)),
+  event: (state) => serializeEvent(state.event),
+  isEditMode: (state) => state.isEditMode,
 };
 
 const mutations = {
   setEvents: (state, events) => (state.events = events),
+  appendEvent: (state, event) => (state.events = [...state.events, event]),
   setEvent: (state, event) => (state.event = event),
-  setEditMode: (state, bool) => (state.isEditMode = bool)
+  setEditMode: (state, bool) => (state.isEditMode = bool),
 };
 
 const actions = {
@@ -39,12 +27,16 @@ const actions = {
     const response = await axios.get(`${apiUrl}/events`);
     commit('setEvents', response.data); // mutationを呼び出す
   },
+  async createEvent({ commit }, event) {
+    const response = await axios.post(`${apiUrl}/events`, event);
+    commit('appendEvent', response.data);
+  },
   setEvent({ commit }, event) {
     commit('setEvent', event);
   },
   setEditMode({ commit }, bool) {
-    commit('setEditMode', bool)
-  }
+    commit('setEditMode', bool);
+  },
 };
 
 export default {
